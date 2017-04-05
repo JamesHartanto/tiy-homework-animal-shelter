fab0dbb8fd140b59c098360a7ac923c4209135ad
James Hartanto <jameshartanto1993@gmail.com>
2017-04-04 23:23:36 -0400

    uncommented Main class code, added arraylist back... wasn't sure if it was supposed to be deleted

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,25 +12,26 @@ public class Main {
         String jdbcUrl = "jdbc:postgresql://localhost/animalshelter";
         AnimalRepository repository = new AnimalRepository(jdbcUrl);
 
-//        MenuService menuService = new MenuService(scanner);
-//        while(true){
-//            int selection = menuService.promptForMainMenu();
-//
-//            if (selection==1){
-//                menuService.listAnimals(AnimalList);
-//            } else if (selection==2){
-//                AnimalList.add(menuService.createAnAnimal());
-//            } else if (selection==3){
-//                menuService.viewAnimal(AnimalList);
-//            } else if (selection==4){
-//                menuService.editAnimal(AnimalList);
-//            } else if (selection==5){
-//                menuService.deleteAnimal(AnimalList);
-//            } else if (selection==6){
-//                if (menuService.exitAnimal()){
-//                    break;
-//                };
-//            }
-//        }
+        MenuService menuService = new MenuService(scanner);
+        ArrayList<Animals> AnimalList = new ArrayList<>();
+        while(true){
+            int selection = menuService.promptForMainMenu();
+
+            if (selection==1){
+                menuService.listAnimals(AnimalList);
+            } else if (selection==2){
+                AnimalList.add(menuService.createAnAnimal());
+            } else if (selection==3){
+                menuService.viewAnimal(AnimalList);
+            } else if (selection==4){
+                menuService.editAnimal(AnimalList);
+            } else if (selection==5){
+                menuService.deleteAnimal(AnimalList);
+            } else if (selection==6){
+                if (menuService.exitAnimal()){
+                    break;
+                };
+            }
+        }
     }
 }
